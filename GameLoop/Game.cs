3733652a20875b80013ada141815d8367e0018ea
iHouqLF <73959649+JoelHer@users.noreply.github.com>
2022-06-06 20:38:27 +0200

    Add files via upload

---
+++ GameLoop/Game.cs
@@ -0,0 +1,52 @@
+﻿using GLFW;
+using OpenGLTest.Rendering.Display;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenGLTest.GameLoop
+{
+    abstract class Game
+    {
+
+        protected int InitialWindowWidth { get; set; }
+        protected int InitialWindowHeight { get; set; } 
+        protected string InitialWindowTitle { get; set; } 
+
+        public Game(int initialWindowWidth, int initialWindowHeight, string initialWindowTitle)
+        {
+            InitialWindowWidth = initialWindowWidth;
+            InitialWindowHeight = initialWindowHeight;
+            InitialWindowTitle = initialWindowTitle;
+        }
+
+        public void Run()
+        {
+            Initialize();
+
+            DisplayManager.CreateWindow(InitialWindowWidth, InitialWindowHeight, InitialWindowTitle);
+
+            LoadContent();
+
+            while (!Glfw.WindowShouldClose(DisplayManager.Window))
+            {
+                GameTime.DeltaTime = (float)Glfw.Time - GameTime.TotalElapsedSeconds;
+                GameTime.TotalElapsedSeconds = (float)Glfw.Time;
+                    
+                Update();
+
+                Glfw.PollEvents();
+
+                Render();
+            }
+
+            DisplayManager.CloseWindows();
+        }
+
+        protected abstract void Initialize();
+        protected abstract void LoadContent();
+
+        protected abstract void Update();
+        protected abstract void Render();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenGLTest.GameLoop;
+
+namespace OpenGLTest
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new TestGame(800, 600, "HelloWorld");
+            game.Run();
+        }
+    }
+}
